7eac17fae40de414b6da11acb59a4c7967303c07
iamhyc <sudofree@163.com>
2022-08-19 15:32:00 +0800

    proc_create mmap build pass for v5.15

--- hack_mmap.c
+++ hack_mmap.c
@@ -9,15 +9,17 @@
 static struct mmap_info *m_info;
 
 /******************** STRUCT MMAP OPERATIONS ********************/
-void mmap_open(struct vm_area_struct *vma)
+static void mmap_open(struct vm_area_struct *vma)
 {
     // info_blk *tmp_info = (mmap_info *)vma->vm_private_data
 }
-void mmap_close(struct vm_area_struct *vma)
+
+static void mmap_close(struct vm_area_struct *vma)
 {
     // info_blk *tmp_info = (mmap_info *)vma->vm_private_data;
 }
-static int mmap_fault(struct vm_fault *vmf)
+
+static vm_fault_t mmap_fault(struct vm_fault *vmf)
 {
     struct page *v_page;
 
@@ -36,18 +38,19 @@ struct vm_operations_struct mmap_vm_ops =
 };
 
 /******************** STRUCT FILE OPERATIONS ********************/
-int mmap_ops_mount(struct file *fd, struct vm_area_struct *vma)
-{
-    vma->vm_ops = &mmap_vm_ops;
-    vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;//VM_RESERVED;
-    vma->vm_private_data = fd->private_data;
-    mmap_open(vma);
-    return 0;
-}
-
 static int fop_open_mmap(struct inode *inode, struct file *fd)
 {
     fd->private_data = m_info;    // attach memory to procfs fd
+    return 0;
+}
+
+static ssize_t default_read_file(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+    return 0;
+}
+
+static ssize_t default_write_file(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
+{
     return 0;
 }
 
@@ -57,24 +60,21 @@ static int fop_close_mmap(struct inode *inode, struct file *fd)
     return 0;
 }
 
-static ssize_t default_read_file(struct file *file, char __user *buf,
-				 size_t count, loff_t *ppos)
+int mmap_ops_mount(struct file *fd, struct vm_area_struct *vma)
 {
+    vma->vm_ops = &mmap_vm_ops;
+    vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;//VM_RESERVED;
+    vma->vm_private_data = fd->private_data;
+    mmap_open(vma);
     return 0;
 }
 
-static ssize_t default_write_file(struct file *file, const char __user *buf,
-				   size_t count, loff_t *ppos)
-{
-    return 0;
-}
-
-static const struct file_operations mmap_fops = {
-    .mmap = mmap_ops_mount,
-    .open = fop_open_mmap,
-    .release = fop_close_mmap,
-    .read = default_read_file,
-    .write = default_write_file,
+static const struct proc_ops mmap_fops = {
+    .proc_open = fop_open_mmap,
+    .proc_read = default_read_file,
+    .proc_write = default_write_file,
+    .proc_release = fop_close_mmap,
+    .proc_mmap = mmap_ops_mount,
 };
 
 /******************** CUSTOMIZED FUNCTIONS ********************/
@@ -94,9 +94,8 @@ inline int mmap_read(char *dst, size_t len)
 
 int hack_mmap_init()
 {
-    //NOTE: everytime the user access the procfs, the same dirty memory would be served.
     m_info = kmalloc(sizeof(struct mmap_info), GFP_KERNEL);
-    m_info->blk = (info_blk *)get_zeroed_page(GFP_KERNEL);
+    m_info->blk = (info_blk *) get_zeroed_page(GFP_KERNEL);
     proc_create(DBGFS_FILE, 0, NULL, &mmap_fops);
     return 0;
 }
